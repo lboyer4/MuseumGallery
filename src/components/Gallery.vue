a961e03bcabc220d0a9125800e69f7778059160e
Lauren Boyer <lboyer4@outlook.com>
2019-07-29 17:39:11 -0600

    Made a header and gallery component

---
+++ src/components/Gallery.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<h1>Gallery</h1>
+		 <div class="wall" v-if="images.length">
+        <img class="image" v-for="(image) in images" v-bind:src="image.baseimageurl" />
+     </div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Gallery",
+	props: ["images"]
+}
+</script>
+
+<style scoped>
+</style>
