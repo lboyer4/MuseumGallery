52f48ba276bee526b155d1bd6b122a844947866a
Lauren Boyer <lboyer4@outlook.com>
2019-07-29 17:45:28 -0600

    Added a gallery component that displays all of the images

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
   <div id="app">
-    <div v-if="images.length">
-      <img v-for="(image) in images" v-bind:src="image.baseimageurl" />
+    <Header  v-bind:msg="msg" />
+    <Gallery v-bind:images="images"/>
+    <div id="slider">
+      <h1>{{msg}}</h1>
+<!--       <div class="wall" v-if="images.length">
+        <img class="image" v-bind:key v-for="(image) in images" v-bind:src="image.baseimageurl" />
+      </div> -->
+   
     </div>
   </div>
 </template>
 
 <script>
 
-import apikey from './assets/apikey.js'
+import apikey from './assets/apikey.js';
+import Header from './components/Header.vue';
+import Gallery from './components/Gallery.vue';
+
 export default {
   name: 'app',
-  data: function() {
-    return {images: []}
+  components: {
+    Header,
+    Gallery
+  },
+  data() {
+    return {
+      images: [],
+      msg: 'Hello'
+    }
   },
   mounted: function() {
     this.getImages()
@@ -29,12 +45,29 @@ export default {
 </script>
 
 <style>
-#app {
+* {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
   color: #2c3e50;
-  margin-top: 60px;
+
 }
+
+.wall {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+body, html {
+  background-image: url("./assets/displaywall.jpeg" );
+  height: 100%;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.image {
+  height: 300px;
+  width: 200px;
+}
+
 </style>
--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,9 +1,9 @@
 <template>
 	<div>
 		<h1>Gallery</h1>
-		 <div class="wall" v-if="images.length">
-        <img class="image" v-for="(image) in images" v-bind:src="image.baseimageurl" />
-     </div>
+		<div class="wall" v-if="images.length">
+      <img class="image" v-for="(image) in images" v-bind:key="image.id" v-bind:src="image.baseimageurl" />
+    </div>
 	</div>
 </template>
 
