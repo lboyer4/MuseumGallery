17300a0aaaeece05b88679b4956baa30e3df1702
Lauren Boyer <lboyer4@outlook.com>
2019-07-30 17:33:07 -0600

    Added buttons to view next and previous images

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,19 +1,34 @@
 <template>
+	<div class="long-strip">
 	<div class="gallery">
-		<div v-bind:key="image.id" v-if="images.length" v-for="image in images">
-      <Piece v-bind:image="image" />
-    </div>
+
+		<button v-on:click="counter -= 1">Previous Image
+    </button>
+		<div>
+      <Piece v-bind:display="images[counter]" />
+     </div>
+    
+    <button v-on:click="counter += 1">Next Image
+    </button>
 	</div>
+</div>
 </template>
 
 <script>
 import Piece from './Piece.vue';
+
 
 export default {
 	name: "Gallery",
 	components: {
 		Piece
 	},
+	data() {
+    return {
+      counter: 0,
+      
+    }
+  },
 	props: ["images"]
 }
 </script>
@@ -22,11 +37,27 @@ export default {
 
 	.gallery {
 		display: flex;
-		overflow-x: scroll;
+		width: 100%;
+		margin-left: 20%;
+	}
+
+	.long-strip {
 		padding: 20px;
 		width: 100%;
 		background-color: #d1d1d1;
-		margin-top: 90px;
+		margin-top: 50px;
+		align-content: center;
+	}
+
+	button {
+		height: 50px;
+		font-size: 20px;
+		width: 160px;
+		margin-top: 150px;
+	}
+
+	button:hover {
+		background-color: #ACB9C4;
 	}
 
 </style>
