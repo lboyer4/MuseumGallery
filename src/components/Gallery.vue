873e5b2a89de81edc4317bce3cfdd9b1849f8bb3
Lauren Boyer <lboyer4@outlook.com>
2019-07-29 18:13:17 -0600

    Images populate in image container

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,18 +1,25 @@
 <template>
 	<div>
 		<h1>Gallery</h1>
-		<div class="wall" v-if="images.length">
-      <img class="image" v-for="(image) in images" v-bind:key="image.id" v-bind:src="image.baseimageurl" />
+		
+		<div v-bind:key="image.id" v-if="images.length" v-for="image in images">
+      <Piece v-bind:image="image" />
     </div>
 	</div>
 </template>
 
 <script>
+import Piece from './Piece.vue';
+
 export default {
 	name: "Gallery",
+	components: {
+		Piece
+	},
 	props: ["images"]
 }
 </script>
 
 <style scoped>
+
 </style>
---
+++ src/components/Piece.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<h1></h1>
+		<img v-bind:src="image.baseimageurl"  />
+<!-- 		<div class="wall" v-if="images.length">
+      <img class="image" v-for="(image) in images" v-bind:key="image.id" v-bind:src="image.baseimageurl" />
+    </div> -->
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Piece",
+	props: ["image"]
+}
+
+</script>
+
+<style scoped>
+	h1 {
+		color: black;
+	}
+</style>
